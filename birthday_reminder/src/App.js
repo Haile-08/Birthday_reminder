b5528bd78ef3b0dcf94435e5de8e27edd16cc1eb
Haile Melaku <haile4cmd@gmail.com>
2021-12-28 18:29:12 +0300

    finished the lsit and app

--- birthday_reminder/src/App.js
+++ birthday_reminder/src/App.js
@@ -1,22 +1,24 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
+
 import './App.css';
+import data from './Data/data';
+import List from './list/list';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-      <header className="App-header">
-          <div className='box container my-auto py-auto  rounded-lg shadow-white'>
-              <h1 className='top font-mono'> 5 Birthdays today</h1>
-               
-               <button className='btn font-mono my-5 py-2 px-20 rounded-lg bg-opacity-70 bg-red-600 hover:bg-red-500 hover:opacity-100 shadow-white'
-               onClick={()=>console.log("clicked")}>Clear all</button>
-          </div>
-      </header>
-    </div>
-    )
-  }
+function App() {
+  const [people,setpeople]= useState(data);
+  return(
+    <div className="App">
+    <header className="App-header">
+        <div className='box container my-auto py-auto  rounded-lg shadow-white'>
+            <h1 className='top font-mono'> {people.length} Birthdays today</h1>
+             <List people={people} />
+             <button className='btn font-mono my-5 py-2 px-20 rounded-lg bg-opacity-70 bg-red-600 hover:bg-red-500 hover:opacity-100 shadow-white'
+             onClick={()=>setpeople([])}>Clear all</button>
+        </div>
+    </header>
+  </div>
+  
+  );
 }
-
 
 export default App;
--- birthday_reminder/src/list/list.js
+++ birthday_reminder/src/list/list.js
@@ -1,14 +1,25 @@
-import React,{Component} from "react";
+import React from "react";
 import './list.css';
 
-class List extends Component{
-    render(){
-        return(
+const List =({people})=>{
+    return(
         <React.Fragment>
-
+            {
+                people.map((person)=>{
+                     const {id,name,age,image} = person;
+                     return(
+                         <article key={id} className="person container font-mono my-3 flex mx-auto">
+                             <img src={image} className="img  h-20 w-20 mx-10 rounded-full"/>
+                             <div>
+                                 <h4 className="name text-base text-center">{name}</h4>
+                                 <p className="age text-sm">{age} years</p>
+                             </div>
+                         </article>
+                     );  
+                })
+            }
         </React.Fragment>
-        )
-    }
+    )
 }
 
 export default List;
